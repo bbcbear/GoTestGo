e5519c1b2f4a1e7f1b5a4cec0cb59dd9051ef25d
bbcbear <bbcbear@yandex.ru>
2020-09-29 13:38:35 +0300

    Update app.go

--- app.go
+++ app.go
@@ -5,20 +5,22 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func search(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
+}
 
+func loader(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
-
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/get", search)              // for search (GET) world in list
+	http.HandleFunc("/load", loader)             // for load (POST) world list
+	log.Fatal(http.ListenAndServe(":8080", nil)) // error log handler
 }
