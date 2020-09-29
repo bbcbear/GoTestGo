874b0de0abc95919d1ef480692353734cd91d433
bbcbear <bbcbear@yandex.ru>
2020-09-29 14:32:49 +0300

    Update app.go

--- app.go
+++ app.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sort"
+	"strings"
 )
 
 func search(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		http.Error(w, "Method is supported.", http.StatusNotFound)
 		return
 	}
 }
 
 func loader(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		log.Print(r.Form) // write in console any post data
-		http.Error(w, "Method POST is not supported.", http.StatusNotFound)
+		r.ParseForm()
+		fmt.Fprintln(w, r.Form)
 		return
 	}
 }
@@ -24,4 +27,15 @@ func main() {
 	http.HandleFunc("/get", search)              // for search (GET) world in list
 	http.HandleFunc("/load", loader)             // for load (POST) world list
 	log.Fatal(http.ListenAndServe(":8080", nil)) // error log handler
+}
+
+func SortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+type Words struct {
+	ID   int
+	Name []string
 }
